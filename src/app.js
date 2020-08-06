abf8a8e5722ad735aa5d26a5c0c05ea5aa0b25ce
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-08-06 19:07:30 -0400

    ready to develop api endpoints

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
+const app = express();
 
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(
+    cors({
+         origin: CLIENT_ORIGIN
+    })
+);
 
-app.get('/', (req,res) => {
-  res.send('Hello, universe!');
+app.get('/api/*', (req,res) => {
+  // res.send('Hello, universe!');
+  res.json({ok: true});
 })
 
 app.use(function errorHandler(error, req, res, next) {
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,10 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET /api/* responds with 200 and json object', () => {
+    const test ={ok: true}
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, universe!')
+      .get('/api/randomendpoint')
+      .expect(200, test)
   })
 })
